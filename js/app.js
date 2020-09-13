a6c5ff80e8510b92259b6052950097affe63e8aa
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-09-13 16:50:22 +0300

    Add the basic structure of the javascript script

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// variables
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+// Cart
+let cart = [];
+
+// getting the products
+class Products {
+
+}
+
+// display products
+class UI {
+
+}
+
+// local storage
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
+});
