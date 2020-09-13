e53ef6bc8af1fb59188d5041d4b7f6ca785ff3c0
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-09-13 19:35:45 +0300

    Dynamically add the rest of the products to the home page

--- js/app.js
+++ js/app.js
@@ -40,7 +40,29 @@ class Products {
 // display products
 class UI {
     displayProducts(products) {
-    	let result = '';
+        let result = '';
+        products.forEach(product => {
+            result += ` 
+    		<!-- single product -->
+			<article class="product">
+				<div class="img-container">
+					<img 
+					src=${product.image} 
+					alt="Product image" 
+					class="product-img">
+					<button class="bag-btn" data-id=${product.id}>
+					<i class="fas fa-shopping-cart"></i> 
+					add to bag
+					</button>
+				</div>
+				<h3>${product.title}</h3>
+				<h4>$${product.price}</h4>
+			</article>
+			<!-- end of single product -->
+    		`;
+        });
+
+        productsDOM.innerHTML = result;
     }
 }
 
