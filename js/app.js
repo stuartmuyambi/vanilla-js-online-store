005792d9cf858a9cc3a19648bd3726309bde54ef
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-10-07 13:42:46 +0300

    Remove item from the cart if its value is less than 0;

--- js/app.js
+++ js/app.js
@@ -164,11 +164,24 @@ class UI {
             } else if (event.target.classList.contains("fa-chevron-up")) {
                 let addAmount = event.target;
                 let id = addAmount.dataset.id;
-                let tempItem = cart.find(item => item.id === id);
+                let tempItem = cart.find((item) => item.id === id);
                 tempItem.amount = tempItem.amount + 1;
-                Storage.saveCart(cart); 
+                Storage.saveCart(cart);
                 this.setCartValues(cart);
                 addAmount.nextElementSibling.innerText = tempItem.amount;
+            } else if (event.target.classList.contains("fa-chevron-down")) {
+                let lowerAmount = event.target;
+                let id = lowerAmount.dataset.id;
+                let tempItem = cart.find((item) => item.id === id);
+                tempItem.amount = tempItem.amount - 1;
+                if (tempItem.amount > 0) {
+                    Storage.saveCart(cart);
+                    this.setCartValues(cart);
+                    lowerAmount.previousElementSibling.innerText = tempItem.amount;
+                } else {
+                    cartContent.removeChild(lowerAmount.parentElement.parentElement);
+                    this.removeItem(id);
+                }
             }
         });
 
