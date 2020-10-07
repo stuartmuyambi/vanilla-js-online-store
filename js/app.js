33ae1d94709cd2d295d92ae01e366af2374debeb
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-10-07 13:15:38 +0300

    Remove items from the cart menu

--- js/app.js
+++ js/app.js
@@ -155,6 +155,16 @@ class UI {
         });
 
         // cart functionality
+        cartContent.addEventListener("click", (event) => {
+            if (event.target.classList.contains("remove-item")) {
+                let removeItem = event.target;
+                let id = removeItem.dataset.id;
+                cartContent.removeChild(removeItem.parentElement.parentElement);
+                this.removeItem(id);
+            }
+        });
+
+
     }
     clearCart() {
         let cartItems = cart.map(item => item.id);
